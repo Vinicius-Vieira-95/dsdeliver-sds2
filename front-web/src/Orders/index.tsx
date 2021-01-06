2cbdb90ad89b5ec62938174c220f273f29a6a49b
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2021-01-06 20:17:35 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+
+import './styles.css';
+import StepsHeader from './StepsHeader';
+
+function Orders(){
+    return(
+       <div className = "orders-container">
+           <StepsHeader />
+       </div>
+    )
+}
+
+export default Orders;
---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, RouterProps, Switch } from "react-router-dom";
+import Home from "./Home";
+import Orders from "./Orders";
+import Navbar from "./Navbar";
+
+function Routes(){
+    return(
+        <BrowserRouter> 
+            <Navbar />
+            <Switch>
+                <Route path = "/orders">
+                    <Orders/>
+                </Route>
+                <Route path = "/">
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter> 
+    )
+}
+
+export default Routes;
