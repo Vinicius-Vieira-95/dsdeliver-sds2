e99e4af0ecd8c7749b8e4a7f003e8430d6d63724
Vinicius Vieira Carneiro <vinicius.95vieira00@gmail.com>
2021-01-06 16:25:21 -0300

    add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
+
 
 function App() {
   return(
-    <div>
+    <>
       <Navbar />
-    </div>
-  )
+      <Home />
+    </>
+  );
 }
 
 export default App;
